56a3b7b9b5d8ec794e0f58daa2819bb0b21339db
1BM17CS150 <54196731+1BM17CS150@users.noreply.github.com>
2019-10-10 17:45:44 +0530

    Update lab_5_bfs.cpp

--- lab_5_bfs.cpp
+++ lab_5_bfs.cpp
@@ -14,7 +14,7 @@ void BFS(int k)
         w=q[f];
         f++;
         cout<<w;
-        for(int i=1;i<=n;i++)
+        for(int i=0;i<n;i++)
         {
          if(a[w][i]==1 && visited[i]==0)
          {
@@ -34,10 +34,10 @@ int main()
     cout << "Enter the number of vertices in the graph : ";
     cin>>n;
     cout<<"Enter the adjacency matrix : "<<endl;
-    for(int i=1;i<=n;i++)
+    for(int i=0;i<n;i++)
     {
 
-        for(int j=1;j<=n;j++)
+        for(int j=0;j<n;j++)
         {
             cin>>a[i][j];
         }
